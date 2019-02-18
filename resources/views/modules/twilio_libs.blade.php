0c747100d27dad0ebbbca97569c82b7eaf4981d1
ymteruya <ym.teruya@lexues.co.jp>
2019-02-18 14:26:17 +0900

    TwilioVideoのバージョンを更新( 1.0.0 => 1.15.2 )

--- resources/views/modules/twilio_libs.blade.php
+++ resources/views/modules/twilio_libs.blade.php
@@ -2,5 +2,5 @@
 <link rel="stylesheet" href="//media.twiliocdn.com/sdk/js/frame/v0.1/twilio-frame.css" type="text/css"></link>
 <script src="//media.twiliocdn.com/sdk/js/frame/v0.1/twilio-frame.min.js"></script>
 <script src="//media.twiliocdn.com/sdk/js/chat/v1.0/twilio-chat.min.js"></script>
-<script src="//media.twiliocdn.com/sdk/js/video/releases/1.0.0/twilio-video.js"></script>
+<script src="//media.twiliocdn.com/sdk/js/video/releases/1.15.2/twilio-video.js"></script>
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
